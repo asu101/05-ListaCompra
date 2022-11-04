a83c907b0c2715cab3ed2fb13353b016267df896
asu <asu101ale@gmail.com>
2022-11-04 09:46:28 +0100

    Create Products

--- app/src/main/java/com/example/a05_listacompra/MainActivity.java
+++ app/src/main/java/com/example/a05_listacompra/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a05_listacompra;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import com.example.a05_listacompra.modelos.Producto;
@@ -7,6 +8,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.LayoutInflater;
 import android.view.View;
 
 
@@ -14,6 +16,8 @@ import com.example.a05_listacompra.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -40,6 +44,22 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private AlertDialog createProducto(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Nuevo producto");
+        builder.setCancelable(false);
+
+        // Necesitamos un conenido
+        View productoView = LayoutInflater.from(this).inflate(R.layout.producto_view_alert, null);
+        EditText txtNombre = productoView.findViewById(R.id.txtNombreProductoAlert);
+        EditText txtCantidad = productoView.findViewById(R.id.txtCantidadProductoAlert);
+        EditText txtPrecio = productoView.findViewById(R.id.txtPrecioProductoAlert);
+        TextView lblTotal = productoView.findViewById(R.id.lblTotalProductoAlert);
+
+        return builder.create();
     }
 
 }
