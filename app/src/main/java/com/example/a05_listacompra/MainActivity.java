709a37ac9a3ec37488b57e99cdcb6df59c9cbb37
asu <asu101ale@gmail.com>
2022-11-04 10:02:36 +0100

    Update price when you add a price and amount

--- app/src/main/java/com/example/a05_listacompra/MainActivity.java
+++ app/src/main/java/com/example/a05_listacompra/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a05_listacompra;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.example.a05_listacompra.modelos.Producto;
@@ -8,6 +9,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -40,13 +42,12 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               createProducto().show();
             }
         });
     }
 
-    private AlertDialog createProducto(){
+    private AlertDialog createProducto() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         builder.setTitle("Nuevo producto");
@@ -58,6 +59,55 @@ public class MainActivity extends AppCompatActivity {
         EditText txtCantidad = productoView.findViewById(R.id.txtCantidadProductoAlert);
         EditText txtPrecio = productoView.findViewById(R.id.txtPrecioProductoAlert);
         TextView lblTotal = productoView.findViewById(R.id.lblTotalProductoAlert);
+        builder.setView(productoView);
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(android.text.Editable s) {
+                String nombre = txtNombre.getText().toString();
+                String cantidad = txtCantidad.getText().toString();
+                String precio = txtPrecio.getText().toString();
+
+                if (nombre.isEmpty() || cantidad.isEmpty() || precio.isEmpty()) {
+                    lblTotal.setText("0.0");
+                } else {
+                    float total = Integer.parseInt(cantidad) * Float.parseFloat(precio);
+                    lblTotal.setText(String.valueOf(total));
+                }
+            }
+        };
+
+        txtCantidad.addTextChangedListener(textWatcher);
+        txtPrecio.addTextChangedListener(textWatcher);
+
+        builder.setPositiveButton("Guardar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (txtNombre.getText().toString().isEmpty() || txtCantidad.getText().toString().isEmpty() || txtPrecio.getText().toString().isEmpty()) {
+                    Producto producto = new Producto(txtNombre.getText().toString(),
+                            Integer.parseInt(txtCantidad.getText().toString()),
+                            Float.parseFloat(txtPrecio.getText().toString()));
+                    productos.add(producto);
+                } else {
+                    Snackbar.make(binding.getRoot(), "No puede haber campos vacios", Snackbar.LENGTH_LONG).show();
+
+
+                    Snackbar.make(binding.getRoot(), "Producto guardado", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+        builder.setNegativeButton("Cancelar", null);
+
 
         return builder.create();
     }
